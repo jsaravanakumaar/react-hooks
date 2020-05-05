415a6d545cdc9f687a9b6bac0fee4dd929722668
Saravanakumaar Jeyabalan <jsarava@gmail.com>
2020-05-05 22:14:49 +0530

    Gauge React component using D3JS library

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Counter from './components/Counter';
+import Gauge from './components/Gauge';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <h2>useEffect - Demo</h2>
         <Counter />
+        <Gauge value="40"/>
       </header>
     </div>
   );
